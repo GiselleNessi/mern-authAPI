2317c08bbb7fb085c7408137815846c86de9b6bd
Giselle Chacon <gisecn@gmail.com>
2021-03-31 17:46:28 +0100

    Added main funtions for users auth and validation

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+// set up server
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.listen(PORT,  () => console.log(`Server started on port: ${PORT}`));
+
+app.use(express.json());
+
+// connect to mongoDB
+
+mongoose.connect(process.env.MDB_CONNECT, {
+
+},
+    (err) => {
+    if (err) return console.error(err);
+    console.log("Connected to MongoDB");
+});
+
+// set up routes
+
+app.use("/auth", require("./routers/userRouter"));
